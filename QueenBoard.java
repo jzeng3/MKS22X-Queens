b3b3ecc22493e41a62153495599f3851d63a3b45
Jamie Zeng <jzeng3@stuy.edu>
2019-02-07 09:02:04 -0500

    coded and tested toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,12 +1,13 @@
 public class QueenBoard{
   public static void main(String[] args){
-
+    QueenBoard testBoard = new QueenBoard(5);
+    System.out.println(testBoard);
   }
 
   private int[][] board;
 
   public QueenBoard(int size){
-
+      board = new int[size][size];
   }
   /**
     *@return The output string formatted as follows:
@@ -20,7 +21,23 @@ public class QueenBoard{
     *(pythonic string notation for clarity,
     *excludes the character up to the *)
     */
-    public String toString(){}
+    public String toString(){
+      String boardStr = "";
+      for (int i = 0; i < board.length; i++){
+        for (int j = 0; j < board[0].length; j++){
+          if (board[i][j] == 0){
+            boardStr += "_";
+          }
+          else{
+            boardStr += "Q";
+          }
+          if (j == board[0].length -1){
+            boardStr += "\n";
+          }
+        }
+      }
+      return boardStr;
+    }
 
 
     /**
@@ -28,14 +45,18 @@ public class QueenBoard{
     *        true when the board is solveable, and leaves the board in a solved state
     *@throws IllegalStateException when the board starts with any non-zero value
     */
-    public boolean solve(){}
+    public boolean solve(){
+      return false;
+    }
 
     /**
     *@return the number of solutions found, and leaves the board filled with only 0's
     *@throws IllegalStateException when the board starts with any non-zero value
     */
-    public int countSolutions(){}
-      
+    public int countSolutions(){
+      return 0;
+    }
+
   private boolean addQueen(int r, int c){
     return true;
   }
