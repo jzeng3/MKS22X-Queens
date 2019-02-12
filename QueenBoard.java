fb194479baada65a80fc11365c2c94ba44d9f9d3
Jamie Zeng <jzeng3@stuy.edu>
2019-02-11 20:40:36 -0500

    Deleted debugging print statements and used loops for testing in main

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,169 +1,151 @@
 import java.util.*;
 public class QueenBoard{
   public static void main(String[] args){
-    //QueenBoard testBoard = new QueenBoard(4);
-    // QueenBoard testBoard = new QueenBoard(5);
-    QueenBoard testBoard = new QueenBoard(8);
-  /*  System.out.println(testBoard);
-    testBoard.addQueen(0,0);
-    System.out.println(testBoard);
-    testBoard.removeQueen(0,0);
-    System.out.println(testBoard);
-    testBoard.solve();*/
-  /*  testBoard.addQueen(1,2);
-    System.out.println(testBoard.toStringDebug());
-    testBoard.removeQueen(1,2);
-    System.out.println(testBoard.toStringDebug());*/
-  //  System.out.println(testBoard.solve());
-    System.out.println(testBoard.countSolutions());
-    System.out.println(testBoard);
-    System.out.println(testBoard.toStringDebug());
-    try{System.out.println(testBoard.solve());
-    }catch(IllegalStateException e){
+    for (int i = 0; i < 15; i++){
+      QueenBoard testBoard = new QueenBoard(i);
+      System.out.println("GENERATED " + i + " X " + i + " BOARD");
+      System.out.println("SOLVEABLE: " + testBoard.solve());
       System.out.println(testBoard);
-      System.out.println("Illegal state exception caught in main");
+      //  System.out.println("NUMBER OF SOLUTIONS: " + testBoard.countSolutions());
+    }
+    System.out.println("NUMBER OF SOLUTIONS FOR");
+    for (int i = 0; i < 15; i++){
+      QueenBoard testBoard = new QueenBoard(i);
+      System.out.println(i + " X " + i + " BOARD: " + testBoard.countSolutions());
     }
   }
+
 
   private int[][] board;
-
+  // Constructor makes a board of given size (N x N)
   public QueenBoard(int size){
-      board = new int[size][size];
+    board = new int[size][size];
   }
   /**
-    *@return The output string formatted as follows:
-    *All numbers that represent queens are replaced with 'Q'
-    *all others are displayed as underscores '_'
-    *There are spaces between each symbol:
-    *"""_ _ Q _
-    *Q _ _ _
-    *_ _ _ Q
-    *_ Q _ _"""
-    *(pythonic string notation for clarity,
-    *excludes the character up to the *)
-    */
-    public String toString(){
-      String boardStr = "";
-      for (int i = 0; i < board.length; i++){
-        for (int j = 0; j < board[0].length; j++){
-          if (board[i][j] >= 0){
-            boardStr += "_ ";
-          }
-          else if (board[i][j] == -1){
-            boardStr += "Q ";
-          }
-          if (j == board[0].length -1){
-            boardStr += "\n";
-          }
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *"""_ _ Q _
+  *Q _ _ _
+  *_ _ _ Q
+  *_ Q _ _"""
+  *(pythonic string notation for clarity,
+  *excludes the character up to the *)
+  */
+  public String toString(){
+    String boardStr = "";
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[0].length; j++){
+        if (board[i][j] >= 0){
+          boardStr += "_ ";
+        }
+        else if (board[i][j] == -1){
+          boardStr += "Q ";
+        }
+        if (j == board[0].length -1){
+          boardStr += "\n";
         }
       }
-      return boardStr;
     }
+    return boardStr;
+  }
 
-    public String toStringDebug(){
-      String boardStr = "";
-      for (int i = 0; i < board.length; i++){
-        for (int j = 0; j < board[0].length; j++){
-            boardStr += board[i][j] + " ";
-          if (j == board[0].length -1){
-            boardStr += "\n";
-          }
+  // prints out the number values of each element of the board
+  public String toStringDebug(){
+    String boardStr = "";
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board[0].length; j++){
+        boardStr += board[i][j] + " ";
+        if (j == board[0].length -1){
+          boardStr += "\n";
         }
       }
-      return boardStr;
     }
+    return boardStr;
+  }
 
 
-    /**
-    *@return false when the board is not solveable and leaves the board filled with zeros;
-    *        true when the board is solveable, and leaves the board in a solved state
-    *@throws IllegalStateException when the board starts with any non-zero value
-    */
-    public boolean solve(){
-      for (int i = 0; i < board.length; i++){
-        if (board[0][i] != 0){
-          throw new IllegalStateException();
-        }
+  /**
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        true when the board is solveable, and leaves the board in a solved state
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public boolean solve(){
+    for (int i = 0; i < board.length; i++){
+      if (board[0][i] != 0){
+        throw new IllegalStateException();
       }
-      return solveH(0);
     }
+    return solveH(0);
+  }
 
-    /**
-    *@return the number of solutions found, and leaves the board filled with only 0's
-    *@throws IllegalStateException when the board starts with any non-zero value
-    */
-    public int countSolutions(){
-      for (int i = 0; i < board.length; i++){
-        if (board[0][i] != 0){
-          throw new IllegalStateException();
-        }
-      }
-      return countSolutionsH(0);
-    }
-    // helper method takes in the current column and the number of queens left
+  // helper method takes in the current column
   private boolean solveH(int colStart){
-    // If reached edge of board, check if all queens fit
-     if (colStart == board.length){
-       return true;
-     }
-     else{
-       // otherwise, loop through each row of the current column
-        for (int row = 0; row < board.length; row++){
-          // check if can add queen at current row, column and add
-          if (addQueen(row, colStart)){
-            addQueen(row, colStart);
-            // check if can SOLVE the rest of the board
-            if (solveH(colStart+1)){
-              return true;
-            }
-            // remove current queen and
-            // move down a row from the previous column
-            removeQueen(row, colStart);
-          }
-        }
-     }
-     return false;
-  }
-  public static String go(int x,int y){
-      return ("\033[" + x + ";" + y + "H");
-  }
-  //And don't forget you can easily delay the printing if needed:
-   public static void wait(int millis){
-       try {
-           Thread.sleep(millis);
-       }
-       catch (InterruptedException e) {
-       }
-   }
-  // helper method takes in the current column and the number of queens left
-private int countSolutionsH(int colStart){
-/*  System.out.println(QueenBoard.go(1,1));
-  System.out.println(this);
-  QueenBoard.wait(1000);*/
-  // If reached edge of board, check if all queens fit
-   if (colStart == board.length){
-     return 1;
-   }
-   int numSolutions = 0;
-     // otherwise, loop through each row of the current column
+    // If reached edge of board, return true (board is solved)
+    if (colStart == board.length){
+      return true;
+    }
+    else{
+      // otherwise, loop through each row of the current column
       for (int row = 0; row < board.length; row++){
-        System.out.println("row "+row+ " colSTart " +colStart);
         // check if can add queen at current row, column and add
         if (addQueen(row, colStart)){
           // check if can SOLVE the rest of the board
-              // removeQueen(row, colStart);
-             numSolutions += countSolutionsH(colStart+1);
-
+          if (solveH(colStart+1)){
+            return true;
           }
           // remove current queen and
           // move down a row from the previous column
-            removeQueen(row, colStart);
+          removeQueen(row, colStart);
+        }
       }
+    }
+    // return false if board cannot be solved
+    return false;
+  }
 
-   return numSolutions;
-}
+  /**
+  *@return the number of solutions found, and leaves the board filled with only 0's
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public int countSolutions(){
+    for (int i = 0; i < board.length; i++){
+      if (board[0][i] != 0){
+        throw new IllegalStateException();
+      }
+    }
+    return countSolutionsH(0);
+  }
 
-// add queen at specified position and mark her targets
+  // helper method takes in the current column
+  private int countSolutionsH(int colStart){
+    /*  System.out.println(QueenBoard.go(1,1));
+    System.out.println(this);
+    QueenBoard.wait(1000);*/
+    // If reached edge of board, solution counts as 1
+    if (colStart == board.length){
+      return 1;
+    }
+    // local variable to keep track of number of solutions
+    int numSolutions = 0;
+    // otherwise, loop through each row of the current column
+    for (int row = 0; row < board.length; row++){
+      // check if can add queen at current row, column and add
+      if (addQueen(row, colStart)){
+        // update the number of solutions with recursive call on next column
+        numSolutions += countSolutionsH(colStart+1);
+
+      }
+      // remove current queen and
+      // move down a row from the previous column
+      removeQueen(row, colStart);
+    }
+    // return the total number of solutions
+    return numSolutions;
+  }
+
+  // add queen at specified position and mark her targets
   private boolean addQueen(int r, int c){
     // check if there is a queen or target, then add queen and targets
     if (board[r][c] == 0){
@@ -191,15 +173,15 @@ private int countSolutionsH(int colStart){
           board[r + i][c + i] += 1;
         }
       }
-
-    return true;
-   }
-   return false;
+      // return true if queen was successfully added, false otherwise
+      return true;
+    }
+    return false;
 
   }
   // remove queen at specified position and her marked targets
   private boolean removeQueen(int r, int c){
-    // check if there is a queen
+    // check if there is a queen at this position
     if (board[r][c] == -1){
       // clear queen
       board[r][c] = 0;
@@ -225,18 +207,21 @@ private int countSolutionsH(int colStart){
           board[r + i][c + i] -= 1;
         }
       }
-
-    return true;
-   }
-   return false;
- }
- // clear and return board to original state
- private void clear(){
-   for (int i = 0; i < board.length; i++){
-     for (int j = 0; j < board[0].length; j++){
-       board[i][j] = 0;
-     }
-   }
- }
-
+      // return true if queen was successfully removed, false otherwise
+      return true;
+    }
+    return false;
+  }
+  // go and wait taken from Mr. K's website, for debugging
+  public static String go(int x,int y){
+    return ("\033[" + x + ";" + y + "H");
+  }
+  //And don't forget you can easily delay the printing if needed:
+  public static void wait(int millis){
+    try {
+      Thread.sleep(millis);
+    }
+    catch (InterruptedException e) {
+    }
+  }
 }
